24e28d4c3ea9ea7883528490d41a72d9428cf03f
Etienne ZIEGELMEYER <zemac@Etiennes-MacBook-Pro.local>
2021-05-01 14:38:20 +0200

    add file todomvc-filter

--- cypress/integration/fnac.spec.js
+++ cypress/integration/fnac.spec.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 describe('todo actions', () => {
+
   beforeEach(() => {
     cy.visit('http://todomvc-app-for-testing.surge.sh/')
     cy.get('.new-todo', {timeout: 6000}).type('Clean room{enter}')
@@ -16,8 +17,9 @@ describe('todo actions', () => {
       cy.get('label').should('have.css', 'text-decoration-line', 'line-through')
   })
 
-    it('should clear completed', () => {
-      cy.contains('Clear completed').click()
+    it('should clear completed todos', () => {
+      cy.get('.toggle').click()
+      cy.contains('Clear').click()
       cy.get('.todo-list').should('not.have.descendants', 'li')
     })
 })
