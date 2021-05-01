8f8b1f196b3811450713a7b6d52dd78ffee5648b
Etienne ZIEGELMEYER <zemac@Etiennes-MacBook-Pro.local>
2021-05-01 14:48:51 +0200

    add test Filter Acrive to do

---
+++ cypress/integration/todomvc-filtering.spaec.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+describe('filtering', () => {
+    beforeEach(() =>{
+        cy.visit('http://todomvc-app-for-testing.surge.sh/')
+
+        cy.get('.new-todo',).type('Clean room{enter}')
+        cy.get('.new-todo',).type('Learn JavaScript{enter}')
+        cy.get('.new-todo',).type('Use Cypress{enter}')
+
+        cy.get('.todo-list li:nth-child(2) .toggle').click()
+
+    })
+
+    it('should Filter "Active" to do',() =>{
+        cy.contains('Active').click()
+    })
+
+
+})
